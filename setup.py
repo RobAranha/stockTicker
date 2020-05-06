ca78fcb69076ca1e56c2642e4af654278d7b23e9
Rob Aranha <rearanha@edu.uwaterloo.ca>
2020-05-05 22:21:35 -0400

    modified setup file

--- setup.py
+++ setup.py
@@ -1,3 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2020 Robert Aranha
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import setup, find_packages
 from codecs import open
 from os import path
@@ -9,16 +26,16 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name = 'StockTicker',
-    version = '1.0.0',
-    description = 'Add stock ticker above desktop tool bar',
-    long_description = long_description,
-    author = 'Robert Aranha',
-    author_email = 'rearanha@uwaterloo.ca',
-    license = 'GNU GPLv3',
-    py_modules = ['DataFetcher', 'PickStocks'],
-    packages = ['StockTicker'],
-    install_requires = [
+    name='StockTicker',
+    version='1.0.0',
+    description='Add stock ticker above desktop tool bar',
+    long_description=long_description,
+    author='Robert Aranha',
+    author_email='rearanha@uwaterloo.ca',
+    license='GNU GPLv3',
+    py_modules=['DataFetcher', 'PickStocks'],
+    packages=['StockTicker'],
+    install_requires=[
         'numpy',
         'yfinance',
         'pandas-datareader',
